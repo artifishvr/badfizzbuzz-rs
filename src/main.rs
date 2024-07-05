536b72a44a304194693d3765f09be4a0a3202f7d
Arti <59352535+artifishvr@users.noreply.github.com>
2024-07-05 11:15:00 -0700

    feat: printihg

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,6 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let query = &args[1] as *const String as i32;
 
-    fizz_buzz::fizzbuzz(query);
+    let fizzbuzz = fizz_buzz::fizzbuzz(query);
+    println!("{}", fizzbuzz);
 }
