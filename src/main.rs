8dee6c7b7d0346067400c53f9cc44bd335821f2b
Arti <59352535+artifishvr@users.noreply.github.com>
2024-07-05 10:10:43 -0700

    its a cli now

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
+use std::env;
 mod fizz_buzz;
 
 fn main() {
-    let n = 1000000;
-    let mut fizzbuzz;
-    for i in 1..=n {
-        fizzbuzz = fizz_buzz::fizzbuzz(i);
-        println!("{}", fizzbuzz);
-    }
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1] as *const String as i32;
+
+    fizz_buzz::fizzbuzz(query);
 }
